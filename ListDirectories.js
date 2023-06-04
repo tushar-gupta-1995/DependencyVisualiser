8a83e9b9abff2e0381557c2aacda59d0b4f0bb8a
tushar-gupta-1995 <125227594+tushar-gupta-1995@users.noreply.github.com>
2023-06-04 18:24:58 +0530

    Update ListDirectories.js

--- ListDirectories.js
+++ ListDirectories.js
@@ -1,4 +1,4 @@
-    // Fetch JSON data from the API endpoint
+
     fetch('http://127.0.0.1:5000/api/directories')
       .then(response => response.json())
       .then(data => {
@@ -6,21 +6,21 @@
 		// log the results
 		console.log(data.name);
 		
-        // Get the element where you want to add the unordered list
+        
         const listContainer = document.getElementById('list-container');
 
-        // Create an unordered list element
+        
         const ul = document.createElement('ul');
 
-        // Iterate over the JSON data and create list items
+      
         data.name.forEach(item => {
 		  console.log(item)
           const li = document.createElement('li');
-          li.textContent = item; // Assuming the JSON has a 'name' property
+          li.textContent = item;
           ul.appendChild(li);
         });
 
-        // Add the unordered list to the container element
+        
         listContainer.appendChild(ul);
       })
       .catch(error => {
