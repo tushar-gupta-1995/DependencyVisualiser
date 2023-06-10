99d47e51d52abdcb3eb78a9c9400a4f2dcf3d95c
tushar-gupta-1995 <guptatushar1995@gmail.com>
2023-06-11 01:00:12 +0530

    removing redundant comments

--- ListTestDirectories.js
+++ ListTestDirectories.js
@@ -1,29 +1,23 @@
 function list_test_directories()
 {
-	// Fetch JSON data from the API endpoint
 	fetch('http://127.0.0.1:5000/api/get_test_directories')
 	  .then(response => response.json())
 	  .then(data => {
 	  
-		// log the results
 		console.log(data.structure);
 		
-		// Get the element where you want to add the unordered list
 		const listContainer = document.getElementById('list-container');
 		listContainer.addEventListener("click", renderTestCode);
 
-		// Create an unordered list element
 		const ul = document.createElement('ul');
 
-		// Iterate over the JSON data and create list items
 		data.structure.forEach(item => {
 		  console.log(item)
 		  const li = document.createElement('li');
-		  li.textContent = item; // Assuming the JSON has a 'name' property
+		  li.textContent = item;
 		  ul.appendChild(li);
 		});
 
-		// Add the unordered list to the container element
 		listContainer.appendChild(ul);
 	  })
 	  .catch(error => {
