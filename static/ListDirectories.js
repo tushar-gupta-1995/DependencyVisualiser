d9155f47f76be5c8e637fb2c31fb6f39a3c6b59f
tushar-gupta-1995 <guptatushar1995@gmail.com>
2023-06-25 17:35:32 +0530

    refining the ui and add option to filter

--- static/ListDirectories.js
+++ static/ListDirectories.js
@@ -1,4 +1,4 @@
-const globalAdjList = new Map();
+let globalAdjList = new Map();
 const visitedTracker = new Map();
 
 fetch('http://127.0.0.1:5000/api/directories')
@@ -8,8 +8,6 @@ fetch('http://127.0.0.1:5000/api/directories')
 
         const listContainer = document.getElementById('list-container');
         listContainer.addEventListener("click", renderGraph);
-        listContainer.addEventListener('contextmenu', showContextMenu);
-        listContainer.addEventListener('click', hideContextMenu);
 
         const ul = document.createElement('ul');
 
@@ -26,8 +24,22 @@ fetch('http://127.0.0.1:5000/api/directories')
         console.error('Error:', error);
     });
 
+const textbox1 = document.getElementById("filter-input");
+const submitBtn = document.getElementById("filter-button");
+
+// Event listener for submit button click
+submitBtn.addEventListener("click", function() {
+    const value1 = textbox1.value;
+
+    // Perform actions with the textbox values
+    console.log("Text Box 1 value: ", value1);
+
+    depthFirstSearchFilteredBroker(value1);
+});
+
 
 function renderGraph(event) {
+    globalAdjList = new Map();
     const clickedElement = event.target;
 
     const apiUrl = 'http://127.0.0.1:5000/api/adjacency-list?folder=' + clickedElement.textContent;
@@ -70,44 +82,18 @@ function renderGraph(event) {
                 depthFirstSearchBroker(clickedNodes[0].label)
             });
 
-
+            printAdjacencyList();
         })
         .catch(error => {
             console.error('Error fetching adjacency list:', error);
         })
 }
 
-const contextMenu = document.getElementById('contextMenu');
-
-function showContextMenu(event) {
-    const clickedElement = event.target;
-    console.log(clickedElement.textContent);
-    event.preventDefault();
-    contextMenu.style.display = 'block';
-    contextMenu.style.left = event.pageX + 'px';
-    contextMenu.style.top = event.pageY + 'px';
-
-    //   fetch('http://127.0.0.1:5000/api/set_test_directories?test_dir='+clickedElement.textContent)
-    contextMenu.onclick = printAdjacencyList
-}
-
-function redirectToTestCasePage() {
-    // window.location.href="{{ url_for('test_documentation_generator') }}"
-    window.location.href = "http://localhost:5000/test_documentation_generator";
-}
-
-
-
-function hideContextMenu() {
-    contextMenu.style.display = 'none';
-    contextMenu.onclick = ""
-}
-
-
 function printAdjacencyList() {
     const json = JSON.stringify(Array.from(globalAdjList), null, 2)
     console.log(json)
     const div = document.getElementById('adjlist');
+    div.textContent = "";
     div.textContent = json;
 }
 
@@ -144,6 +130,46 @@ function depthFirstSearchBroker(node) {
 
 }
 
+
+function depthFirstSearchFilteredBroker(includeText) {
+    for (let [key, value] of globalAdjList) {
+        visitedTracker.set(key, false)
+    }
+
+    let spAdjList = new Map();
+
+    for (let [key, value] of globalAdjList) {
+        if (visitedTracker.get(key))
+            continue;
+        depthFirstSearchFiltered(key, spAdjList, includeText)
+    }
+
+    const nodes = new vis.DataSet();
+    const edges = [];
+    //const nodesInteract = new vis.DataSet();
+
+    for (let [key, value] of spAdjList) {
+        nodes.add({ id: key, label: key });
+
+        spAdjList.get(key).forEach(target => {
+            edges.push({ from: key, to: target, arrows: 'to' });
+        });
+
+    }
+
+    const data = {
+        nodes: nodes,
+        edges: edges
+    };
+
+    const options = getOptions(nodes, edges)
+
+    const container = document.getElementById("graph");
+    const network = new vis.Network(container, data, options);
+
+}
+
+
 function depthFirstSearch(node, spAdjList) {
     if (visitedTracker.get(node)) {
         return;
@@ -165,6 +191,40 @@ function depthFirstSearch(node, spAdjList) {
     });
 }
 
+
+function depthFirstSearchFiltered(node, spAdjList, includeText) {
+
+    console.log(" node: " + node)
+    if (visitedTracker.get(node)) {
+        return;
+    }
+
+    visitedTracker.set(node, true);
+
+    let listElements = globalAdjList.get(node);
+
+    if (node == "nil")
+        return
+
+    listElements.forEach(item => {
+        console.log("connected node: " + item)
+        console.log("includes? " + includeText)
+        if (item.includes(includeText)) {
+            console.log(node + " - " + item)
+            if (spAdjList.get(node) == null)
+                spAdjList.set(node, [item])
+            else
+                spAdjList.set(node, spAdjList.get(node).concat(item));
+
+            if (spAdjList.get(item) == null)
+                spAdjList.set(item, ["nil"])
+        }
+
+        depthFirstSearchFiltered(item, spAdjList, includeText);
+    });
+}
+
+
 function getOptions(nodes, edges) {
 
     return {
@@ -181,4 +241,5 @@ function getOptions(nodes, edges) {
             }
         }
     };
+
 }
